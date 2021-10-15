0cd430fee0377f2c6e3f1d4929d57b233b53efcf
abdec2 <abdec2@hotmail.com>
2021-10-15 19:06:07 +0300

    fix cors

--- src/components/Devs.js
+++ src/components/Devs.js
@@ -10,7 +10,7 @@ import DevItem from "./DevItem"
 function Devs() {
     const opt = ['English', 'Urdu', 'Hindi', 'African']
     const { isLoading, data } = useQuery('devs', () => {
-        return axios.get('https://gh-trending-api.herokuapp.com/developers')
+        return axios.get('https://cors-anywhere.herokuapp.com/https://gh-trending-api.herokuapp.com/developers')
     })
     console.log(data)
     return (
